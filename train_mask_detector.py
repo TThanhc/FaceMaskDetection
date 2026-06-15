a260f9fb3b60b1a77691df8431f569076bec7116
TThanhc <caotienthanh1103@gmail.com>
2026-06-15 09:00:23 +0700

    fix: augemetation only for training

--- train_mask_detector.py
+++ train_mask_detector.py
@@ -1,10 +1,9 @@
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.applications import MobileNetV2
-from tensorflow.keras.layers import AveragePooling2D, Dropout, Flatten, Dense, Input
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dropout, Dense, Input
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
-import os
 
 # Hyperparameters
 INIT_LR = 1e-4
@@ -28,6 +27,12 @@ train_datagen = ImageDataGenerator(
     validation_split=0.2 # 20% for validation set
 )
 
+# No data augmentation for Validation dataset (only preprocessing)
+val_datagen = ImageDataGenerator(
+    preprocessing_function=preprocess_input, # Scaling for MobileNet
+    validation_split=0.2 # 20% for validation set
+)
+
 # Train dataset (80%)
 train_generator = train_datagen.flow_from_directory(
     DIRECTORY,
@@ -35,18 +40,22 @@ train_generator = train_datagen.flow_from_directory(
     batch_size=BS,
     class_mode="categorical",
     subset="training",
+    seed=42,
     shuffle=True
 )
 
 # Validation set (20%)
-val_generator = train_datagen.flow_from_directory(
+val_generator = val_datagen.flow_from_directory(
     DIRECTORY,
     target_size=(224, 224),
     batch_size=BS,
     class_mode="categorical",
     subset="validation",
+    seed=42,
     shuffle=False
 )
+
+print(f"[INFO] Class Indices Mapping: {train_generator.class_indices}")
 
 # Building model
 print("[INFO] Building model...")
@@ -54,8 +63,7 @@ baseModel = MobileNetV2(weights="imagenet", include_top=False,
 	input_tensor=Input(shape=(224, 224, 3)))
 
 headModel = baseModel.output
-headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
-headModel = Flatten(name="flatten")(headModel)
+headModel = GlobalAveragePooling2D()(headModel)
 headModel = Dense(128, activation="relu")(headModel)
 headModel = Dropout(0.5)(headModel)
 headModel = Dense(2, activation="softmax")(headModel)
@@ -65,20 +73,18 @@ model = Model(inputs=baseModel.input, outputs=headModel)
 for layer in baseModel.layers:
 	layer.trainable = False
 
-# Trainning model
-print("[INFO] Compiling and Trainning model...")
+# Training model
+print("[INFO] Compiling and Training model...")
 opt = Adam(learning_rate=INIT_LR)
 model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
 
 H = model.fit(
     train_generator,
-    steps_per_epoch=train_generator.samples // BS,
     validation_data=val_generator,
-    validation_steps=val_generator.samples // BS,
     epochs=EPOCHS
 )
 
 # Saving model
 print("[INFO] Saving model...")
 model.save("mask_detector.h5")
-print("Done!")
+print("Done!")
